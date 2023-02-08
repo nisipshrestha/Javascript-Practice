4e7405582900f853fb5d4b3b7c78fe1f61736595
Nisip Shresth <nisip.shrestha@swifttech.com.np>
2023-02-08 10:48:52 +0545

    Added locales examples

--- Example 4/dateTimeExample.js
+++ Example 4/dateTimeExample.js
@@ -8,6 +8,16 @@
 const currDate =Date.now();
 // console.log(new Date(currDate));
 // console.log(new Date().toDateString());
+
+/* WORKS IN NODE 14+ */
+console.log('UK locale (Great Britain)', new Date().toLocaleString("en-GB"));
+console.log('US locale', new Date().toLocaleString("en-US"));
+console.log('New Zealand locale', new Date().toLocaleString("en-NZ"));
+console.log('South Africa locale', new Date().toLocaleString("en-ZA"));
+
+console.log('utc date', new Date().toUTCString());
+console.log('iso date', new Date().toISOString());
+
 // console.log(new Date().toTimeString());
 
 console.log(new Date().getMonth());
